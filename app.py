44979104bc5998e191fdba3e95ce82b9be158be7
TearfulSeal <153215316+TearfulSeal@users.noreply.github.com>
2024-09-14 20:11:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,29 @@ from translate import Translator
 import requests
 import json
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
+
+chuck_list = os.listdir("static/img")
 
 wsgi_app = app.wsgi_app
 def translate(text):
     translator= Translator(to_lang="ru")
-    translation = translator.translate(text)
-    return translation
-
+    try:
+        translation = translator.translate(text)
+        return translation
+    except:
+        return "bad connection"
 def getQuote():
-    url="https://api.chucknorris.io/jokes/random"
-    return requests.get(url).json()["value"]
-
+    try:
+        url="https://api.chucknorris.io/jokes/random"
+        return requests.get(url).json()["value"]
+    except:
+        return "bad connection"
 @app.route('/')
 def hello():
     quote = getQuote()
-    return render_template("index.html",quote=translate(quote),eng_quote = quote, random = choice(range(1,4)))
+    return render_template("index.html",quote=translate(quote),eng_quote = quote, random = choice(chuck_list))
 
 if __name__ == '__main__':
     import os
